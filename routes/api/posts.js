e1ba95c3f0692c4744b7bf7b138a115f8a0e3c74
timremmert <tbr23@scarletmail.rutgers.edu>
2018-06-22 16:58:13 -0400

    added posts API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
+
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/posts');
 
 /**
  * @route GET api/posts/test
@@ -8,5 +15,50 @@ const router = express.Router();
  * @access Public
  */
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+/**
+ * @route GET api/posts
+ * @desc get posts
+ * @access Public
+ */
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ npostfound: 'No post found with that id' }));
+});
+
+/**
+ * @route GET api/posts/:id
+ * @desc get posts by id
+ * @access Public
+ */
+router.get('/:id', (req, res) => {
+  Post.find()
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ npostfound: 'No posts found' }));
+});
+
+/**
+ * @route POST api/posts
+ * @desc create post
+ * @access Private
+ */
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id,
+  });
+
+  newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
