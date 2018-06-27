0887512ad7127adf7d1670dab5115a656c1c40c3
timremmert <tbr23@scarletmail.rutgers.edu>
2018-06-27 15:11:42 -0400

    added dashboard and profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import jwtDecode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearProfile } from './actions/profileActions';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import store from './store/storeConfig';
 import './App.css';
 
@@ -27,7 +29,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    //todo: clear current profile
+    // clear current profile
+    store.dispatch(clearProfile());
     // redirect to login
     window.location.href = '/login';
   }
@@ -42,6 +45,7 @@ const App = (
         <div className="container">
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </div>
         <Footer />
       </div>
