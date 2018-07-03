eaa4b03cb17ce7c7c468ec441607f550b8484481
timremmert <tbr23@scarletmail.rutgers.edu>
2018-07-03 11:32:40 -0400

    added delete account functionality

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profileActions';
+import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
+  }
+
+  onDeleteClick = () => {
+    this.props.deleteAccount();
   }
 
   render() {
@@ -17,20 +22,26 @@ class Dashboard extends Component {
     let dashboardContent;
 
     if (profile === null || loading) {
-      dashboardContent = <Spinner />
+      dashboardContent = <Spinner />;
+    } else if (Object.keys(profile).length > 0) {
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}> {user.name}</Link></p>
+          <ProfileActions />
+          {/* Todo exp and edu */}
+          <div style={{ marginBottom: '60px' }}></div>
+          <button onClick={this.onDeleteClick} className="btn btn-danger">Delete My Account</button>
+        </div>
+      );
     } else {
-      if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
-      } else {
-        // user is logged in but has no profile
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">Welcome {user.name} </p>
-            <p> You have not yet setup a profile, please add some info </p>
-            <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
-          </div>
-        )
-      }
+      // user is logged in but has no profile
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name} </p>
+          <p> You have not yet setup a profile, please add some info </p>
+          <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+        </div>
+      );
     }
     return (
       <div className="dashboard">
@@ -47,16 +58,17 @@ class Dashboard extends Component {
   }
 }
 
-Dashboard.PropTypes = {
+Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
-}
+};
 
 const mapStateToProps = state => ({
   profile: state.profile,
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
 
